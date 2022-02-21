59bc8c9d80b8f7b32eec9ccec6e8bc7b684d9857
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-02-21 17:10:32 +0100

    refa (utils.benchmark-performance.mk): trivial rewording for the sake of clarity

--- make-tasks/utils.benchmark-performance.mk
+++ make-tasks/utils.benchmark-performance.mk
@@ -10,12 +10,12 @@ define benchmark-performance =
                                                                                                       \
     && operationsMax=`                    awk -F','  'BEGIN{a=0}{ if ($$3>0+a) a=$$3} END{print a}'     "./$(1)---benchmark-output-parsed.dat"    `  \
     && nanosecondsMax=`                   awk -F','  'BEGIN{a=0}{ if ($$4>0+a) a=$$4} END{print a}'     "./$(1)---benchmark-output-parsed.dat"    `  \
-    && bytesMax=`                         awk -F','  'BEGIN{a=0}{ if ($$5>0+a) a=$$5} END{print a}'     "./$(1)---benchmark-output-parsed.dat"    `  \
+    && ramBytesMax=`                      awk -F','  'BEGIN{a=0}{ if ($$5>0+a) a=$$5} END{print a}'     "./$(1)---benchmark-output-parsed.dat"    `  \
     && allocationsMax=`                   awk -F','  'BEGIN{a=0}{ if ($$6>0+a) a=$$6} END{print a}'     "./$(1)---benchmark-output-parsed.dat"    `  \
                                                                                                                                                      \
     && operationsMaxRoundedUpwards=`      awk -v n="$${operationsMax}"      'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
     && nanosecondsMaxRoundedUpwards=`     awk -v n="$${nanosecondsMax}"     'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
-    && bytesMaxRoundedUpwards=`           awk -v n="$${bytesMax}"           'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
+    && ramBytesMaxRoundedUpwards=`        awk -v n="$${ramBytesMax}"        'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
     && allocationsMaxRoundedUpwards=`     awk -v n="$${allocationsMax}"     'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
                                                                                                                                                      \
     && gnuplot                                                                                        \
@@ -39,13 +39,13 @@ define benchmark-performance =
             '../plot.gp'                                                                              \
                                                                                                       \
     && gnuplot                                                                                        \
-            -e "file_path='./$(1)---benchmark-output-parsed.dat'                       "              \
-            -e "graphic_file_name='../../arena-results/$(1)--bytes-per-operation.png'  "              \
-            -e "y_label='bytes / operation'                                            "              \
-            -e "y_range_min='0000''                                                    "              \
-            -e "y_range_max='$${bytesMaxRoundedUpwards}'                               "              \
-            -e "column_1=1                                                             "              \
-            -e "column_2=5                                                             "              \
+            -e "file_path='./$(1)---benchmark-output-parsed.dat'                           "          \
+            -e "graphic_file_name='../../arena-results/$(1)--ram-bytes-per-operation.png'  "          \
+            -e "y_label='ram-bytes / operation'                                            "          \
+            -e "y_range_min='0000''                                                        "          \
+            -e "y_range_max='$${ramBytesMaxRoundedUpwards}'                                "          \
+            -e "column_1=1                                                                 "          \
+            -e "column_2=5                                                                 "          \
             '../plot.gp'                                                                              \
                                                                                                       \
     && gnuplot                                                                                        \
