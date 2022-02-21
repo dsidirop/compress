73eb4ba05ee9bb66ec5d9d38caf7201fb0153696
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-02-21 14:15:05 +0100

    feat (render benchmark results): the makefile system will now automatically deduce the maximum y-value for each plot and set the y-axis max-value accordingly

--- make-tasks/utils.benchmark-eventual-message-size-footprint.mk
+++ make-tasks/utils.benchmark-eventual-message-size-footprint.mk
@@ -7,13 +7,17 @@ define benchmark-serialization-eventual-message-size-footprint =
               '/[*][*]/{count++; printf("%d,%s,%s\n", count, $$4, $$5); }'            \
                 './$(1)--benchmark-raw-output.dat'                                    \
               > './$(1)--benchmark-output-parsed.dat'                                 \
+                                                                                      \
+    && messageSizeMax=`                    awk -F','  'BEGIN{a=0}{ if ($$3>0+a) a=$$3} END{print a}'     "./$(1)--benchmark-output-parsed.dat"     `  \
+                                                                                                                                                      \
+    && messageSizeMaxRoundedUpwards=`      awk -v n="$${messageSizeMax}"      'BEGIN{ print int((n+100) / 100 ) * 100 }'                           `  \
                                                                                                                      \
     && gnuplot                                                                                                       \
             -e "    file_path='./$(1)--benchmark-output-parsed.dat'                                      "           \
             -e "    graphic_file_name='../../arena-results/$(1)--eventual-message-size-footprint.png'    "           \
             -e "    y_label='bytes'                                                                      "           \
             -e "    y_range_min='0000''                                                                  "           \
-            -e "    y_range_max='2000'                                                                   "           \
+            -e "    y_range_max='$${messageSizeMaxRoundedUpwards}'                                       "           \
             -e "    column_1=1                                                                           "           \
             -e "    column_2=3                                                                           "           \
             '../plot.gp'                                                                                             \
--- make-tasks/utils.benchmark-performance.mk
+++ make-tasks/utils.benchmark-performance.mk
@@ -5,15 +5,25 @@ define benchmark-performance =
                                                                                                       \
     && awk                                                                                            \
               '/Benchmark_/{count++; gsub(/Benchmark_+.*?_+/, ""); gsub(/[-][0-9]+ /, ""); printf("%d,%s,%s,%s,%s,%s\n", count, $$1, $$2, $$3, $$5, $$7); }'    \
-                    ./$(1)--benchmark-raw-output.dat                                                  \
-                  > ./$(1)--benchmark-output-parsed.dat                                               \
+                    "./$(1)--benchmark-raw-output.dat"                                                \
+                  > "./$(1)--benchmark-output-parsed.dat"                                             \
                                                                                                       \
+    && operationsMax=`                    awk -F','  'BEGIN{a=0}{ if ($$3>0+a) a=$$3} END{print a}'     "./$(1)--benchmark-output-parsed.dat"     `  \
+    && nanosecondsMax=`                   awk -F','  'BEGIN{a=0}{ if ($$4>0+a) a=$$4} END{print a}'     "./$(1)--benchmark-output-parsed.dat"     `  \
+    && bytesMax=`                         awk -F','  'BEGIN{a=0}{ if ($$5>0+a) a=$$5} END{print a}'     "./$(1)--benchmark-output-parsed.dat"     `  \
+    && allocationsMax=`                   awk -F','  'BEGIN{a=0}{ if ($$6>0+a) a=$$6} END{print a}'     "./$(1)--benchmark-output-parsed.dat"     `  \
+                                                                                                                                                     \
+    && operationsMaxRoundedUpwards=`      awk -v n="$${operationsMax}"      'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
+    && nanosecondsMaxRoundedUpwards=`     awk -v n="$${nanosecondsMax}"     'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
+    && bytesMaxRoundedUpwards=`           awk -v n="$${bytesMax}"           'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
+    && allocationsMaxRoundedUpwards=`     awk -v n="$${allocationsMax}"     'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `  \
+                                                                                                                                                     \
     && gnuplot                                                                                        \
             -e "file_path='./$(1)--benchmark-output-parsed.dat'                    "                  \
             -e "graphic_file_name='../../arena-results/$(1)--operations-count.png' "                  \
             -e "y_label='operations#'                                              "                  \
             -e "y_range_min='0000000''                                             "                  \
-            -e "y_range_max='1500000'                                              "                  \
+            -e "y_range_max='$${operationsMaxRoundedUpwards}'                      "                  \
             -e "column_1=1                                                         "                  \
             -e "column_2=3                                                         "                  \
             ../plot.gp                                                                                \
@@ -23,7 +33,7 @@ define benchmark-performance =
             -e "graphic_file_name='../../arena-results/$(1)--time-per-operation.png' "                \
             -e "y_label='nanoseconds / operation'                                    "                \
             -e "y_range_min='00000''                                                 "                \
-            -e "y_range_max='5000'                                                   "                \
+            -e "y_range_max='$${nanosecondsMaxRoundedUpwards}'                       "                \
             -e "column_1=1                                                           "                \
             -e "column_2=4                                                           "                \
             ../plot.gp                                                                                \
@@ -33,7 +43,7 @@ define benchmark-performance =
             -e "graphic_file_name='../../arena-results/$(1)--bytes-per-operations.png' "              \
             -e "y_label='bytes / operation'                                            "              \
             -e "y_range_min='0000''                                                    "              \
-            -e "y_range_max='2000'                                                     "              \
+            -e "y_range_max='$${bytesMaxRoundedUpwards}'                               "              \
             -e "column_1=1                                                             "              \
             -e "column_2=5                                                             "              \
             ../plot.gp                                                                                \
@@ -43,7 +53,7 @@ define benchmark-performance =
             -e "graphic_file_name='../../arena-results/$(1)--allocations-per-operations.png' "        \
             -e "y_label='allocations / operation'                                            "        \
             -e "y_range_min='000''                                                           "        \
-            -e "y_range_max='40'                                                             "        \
+            -e "y_range_max='$${allocationsMaxRoundedUpwards}'                               "        \
             -e "column_1=1                                                                   "        \
             -e "column_2=6                                                                   "        \
             ../plot.gp                                                                                \
