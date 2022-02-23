7963a8d544a558df3521bc45f496cf2fe1634084
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-02-23 19:51:55 +0100

    feat (benchmarks): allow the calling environment to set the number of cpus# via cpucount=X

--- make-tasks/tasks.benchmark.mk
+++ make-tasks/tasks.benchmark.mk
@@ -1,3 +1,5 @@
+cpucount ?= 1
+
 .PHONY:\
 benchmark
 benchmark:									               \
@@ -11,29 +13,29 @@ benchmark:									               \
 .PHONY:\
 benchmark-serialization-performance
 benchmark-serialization-performance:
-	@$(call benchmark-performance,a-serialization-performance,1)
+	@$(call benchmark-performance,a-serialization-performance,$(cpucount))
 
 .PHONY:\
 benchmark-deserialization-performance
 benchmark-deserialization-performance:
-	@$(call benchmark-performance,b-deserialization-performance,1)
+	@$(call benchmark-performance,b-deserialization-performance,$(cpucount))
 
 .PHONY:\
 benchmark-serialization-deserialization-performance
 benchmark-serialization-deserialization-performance:
-	@$(call benchmark-performance,c-serialization-deserialization-performance,1)
+	@$(call benchmark-performance,c-serialization-deserialization-performance,$(cpucount))
 
 .PHONY:\
 benchmark-serialization-deserialization-elapsed-time
 benchmark-serialization-deserialization-elapsed-time:
-	@$(call benchmark-single-metric,d-serialization-deserialization-elapsed-time,Average Elapsed Time in nsecs\\n(lower is better),ns,1)
+	@$(call benchmark-single-metric,d-serialization-deserialization-elapsed-time,Average Elapsed Time in nsecs\\n(lower is better),ns,$(cpucount))
 
 .PHONY:\
 benchmark-serialization-message-size-footprint
 benchmark-serialization-message-size-footprint:
-	@$(call benchmark-single-metric,e-serialization-eventual-message-size-footprint,Average Size in Bytes\\n(lower is better),bytes,1)
+	@$(call benchmark-single-metric,e-serialization-eventual-message-size-footprint,Average Size in Bytes\\n(lower is better),bytes,$(cpucount))
 
 .PHONY:\
 merge-output-images-of-plots
 merge-output-images-of-plots: # merge all images into one
-	@convert -append       ./arena-results/*-cpu1----category-overall-results.png      ./arena-results/x-cpu1-all-results.png
+	@convert -append       ./arena-results/*-cpu$(cpucount)----category-overall-results.png      ./arena-results/x-cpu$(cpucount)-all-results.png
