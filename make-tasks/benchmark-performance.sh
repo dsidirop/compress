067638cbf02075358de15a24cd50d7b0d5a249b8
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-02-23 19:02:19 +0100

    clean (benchmark-*.sh): trivial neutral tweaks

--- make-tasks/benchmark-performance.sh
+++ make-tasks/benchmark-performance.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-cd "./arena/${1}/"
+benchmark_dirname="${1}"
 
-go  test  -benchmem  -cpu=1   -bench=.   |    tee "./${1}---benchmark-raw-output.dat"
+cd "./arena/${benchmark_dirname}/"
+
+go  test  -benchmem  -cpu=1   -bench=.   |    tee "./${benchmark_dirname}---benchmark-raw-output.dat"
 
 if [[ ${PIPESTATUS[0]} -gt 0 ]]; then
         exit 1
 fi
 
-awk \
-'/Benchmark_/{count++; gsub(/Benchmark_+.*?_+/, ""); gsub(/[-][0-9]+ /, ""); printf("%d,%s,%s,%s,%s,%s\n", count, $1, $2, $3, $5, $7); }' \
-"./${1}---benchmark-raw-output.dat" \
-> "./${1}---benchmark-output-parsed.dat"
 
-# operationsMax=`                    awk -F','                        'BEGIN{a=0}{ if ($3>0+a) a=$3} END{print a}'     "./${1}---benchmark-output-parsed.dat"    `
-# nanosecondsMax=`                   awk -F','                        'BEGIN{a=0}{ if ($4>0+a) a=$4} END{print a}'     "./${1}---benchmark-output-parsed.dat"    `
-# ramBytesMax=`                      awk -F','                        'BEGIN{a=0}{ if ($5>0+a) a=$5} END{print a}'     "./${1}---benchmark-output-parsed.dat"    `
-# allocationsMax=`                   awk -F','                        'BEGIN{a=0}{ if ($6>0+a) a=$6} END{print a}'     "./${1}---benchmark-output-parsed.dat"    `
+
+awk                                                                                                                                        \
+'/Benchmark_/{count++; gsub(/Benchmark_+.*?_+/, ""); gsub(/[-][0-9]+ /, ""); printf("%d,%s,%s,%s,%s,%s\n", count, $1, $2, $3, $5, $7); }'  \
+"./${benchmark_dirname}---benchmark-raw-output.dat"                                                                                        \
+> "./${benchmark_dirname}---benchmark-output-parsed.dat"
+
+# operationsMax=`                    awk -F','                        'BEGIN{a=0}{ if ($3>0+a) a=$3} END{print a}'     "./${benchmark_dirname}---benchmark-output-parsed.dat"    `
+# nanosecondsMax=`                   awk -F','                        'BEGIN{a=0}{ if ($4>0+a) a=$4} END{print a}'     "./${benchmark_dirname}---benchmark-output-parsed.dat"    `
+# ramBytesMax=`                      awk -F','                        'BEGIN{a=0}{ if ($5>0+a) a=$5} END{print a}'     "./${benchmark_dirname}---benchmark-output-parsed.dat"    `
+# allocationsMax=`                   awk -F','                        'BEGIN{a=0}{ if ($6>0+a) a=$6} END{print a}'     "./${benchmark_dirname}---benchmark-output-parsed.dat"    `
 
 # operationsMaxRoundedUpwards=`      awk -v n="${operationsMax}"      'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `
 # nanosecondsMaxRoundedUpwards=`     awk -v n="${nanosecondsMax}"     'BEGIN{ print int((n+100) / 100 ) * 100 }'                            `
@@ -26,55 +30,55 @@ awk \
 tempDir=`mktemp -d -t golang-compression-libs-arena.XXXX`
 tempPlotConfigFile="${tempDir}/plot.gp"
 
-cp            '../plot.gp'                                                     "${tempPlotConfigFile}"
-sed    -i     "s/___TITLE___/[${1}] CPU Operations#\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
+cp            '../plot.gp'                                                                     "${tempPlotConfigFile}"
+sed    -i     "s/___TITLE___/[${benchmark_dirname}] CPU Operations#\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
 gnuplot \
--e "file_path='./${1}---benchmark-output-parsed.dat'                                        "  \
--e "graphic_file_name='../../arena-results/${1}--cpu-operations-count--result.png'          "  \
--e "y_label='cpu-ops#'                                                                      "  \
--e "column_1=1                                                                              "  \
--e "column_2=3                                                                              "  \
+-e "file_path='./${benchmark_dirname}---benchmark-output-parsed.dat'                                        "  \
+-e "graphic_file_name='../../arena-results/${benchmark_dirname}--cpu-operations-count--result.png'          "  \
+-e "y_label='cpu-ops#'                                                                                      "  \
+-e "column_1=1                                                                                              "  \
+-e "column_2=3                                                                                              "  \
 "${tempPlotConfigFile}"
 
-cp            '../plot.gp'                                                             "${tempPlotConfigFile}"
-sed    -i     "s/___TITLE___/[${1}] Time (ns) per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
+cp            '../plot.gp'                                                                     "${tempPlotConfigFile}"
+sed    -i     "s/___TITLE___/[${benchmark_dirname}] Time (ns) per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
 gnuplot \
--e "file_path='./${1}---benchmark-output-parsed.dat'                                        "  \
--e "graphic_file_name='../../arena-results/${1}--time-per-operation--result.png'            "  \
--e "y_label='nanoseconds / operation'                                                       "  \
--e "column_1=1                                                                              "  \
--e "column_2=4                                                                              "  \
+-e "file_path='./${benchmark_dirname}---benchmark-output-parsed.dat'                                        "  \
+-e "graphic_file_name='../../arena-results/${benchmark_dirname}--time-per-operation--result.png'            "  \
+-e "y_label='nanoseconds / operation'                                                                       "  \
+-e "column_1=1                                                                                              "  \
+-e "column_2=4                                                                                              "  \
 "${tempPlotConfigFile}"
 
-cp            '../plot.gp'                                                             "${tempPlotConfigFile}"
-sed    -i     "s/___TITLE___/[${1}] RAM Bytes per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
+cp            '../plot.gp'                                                                     "${tempPlotConfigFile}"
+sed    -i     "s/___TITLE___/[${benchmark_dirname}] RAM Bytes per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
 gnuplot \
--e "file_path='./${1}---benchmark-output-parsed.dat'                                        "  \
--e "graphic_file_name='../../arena-results/${1}--ram-bytes-per-operation--result.png'       "  \
--e "y_label='ram-bytes / operation'                                                         "  \
--e "column_1=1                                                                              "  \
--e "column_2=5                                                                              "  \
+-e "file_path='./${benchmark_dirname}---benchmark-output-parsed.dat'                                        "  \
+-e "graphic_file_name='../../arena-results/${benchmark_dirname}--ram-bytes-per-operation--result.png'       "  \
+-e "y_label='ram-bytes / operation'                                                                         "  \
+-e "column_1=1                                                                                              "  \
+-e "column_2=5                                                                                              "  \
 "${tempPlotConfigFile}"
 
-cp            '../plot.gp'                                                               "${tempPlotConfigFile}"
-sed    -i     "s/___TITLE___/[${1}] Allocations per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
+cp            '../plot.gp'                                                                       "${tempPlotConfigFile}"
+sed    -i     "s/___TITLE___/[${benchmark_dirname}] Allocations per Operation\\\\n(lower is better)/g"   "${tempPlotConfigFile}"
 gnuplot \
--e "file_path='./${1}---benchmark-output-parsed.dat'                                        "  \
--e "graphic_file_name='../../arena-results/${1}--allocations-per-operation--result.png'     "  \
--e "y_label='allocations / operation'                                                       "  \
--e "column_1=1                                                                              "  \
--e "column_2=6                                                                              "  \
+-e "file_path='./${benchmark_dirname}---benchmark-output-parsed.dat'                                        "  \
+-e "graphic_file_name='../../arena-results/${benchmark_dirname}--allocations-per-operation--result.png'     "  \
+-e "y_label='allocations / operation'                                                                       "  \
+-e "column_1=1                                                                                              "  \
+-e "column_2=6                                                                                              "  \
 "${tempPlotConfigFile}"
 
 
 montage                                                                 \
             -mode concatenate                                           \
-            "../../arena-results/${1}--*--result.png"                   \
-            "../../arena-results/${1}---category-overall-results.png"
+            "../../arena-results/${benchmark_dirname}--*--result.png"   \
+            "../../arena-results/${benchmark_dirname}---category-overall-results.png"
 
 
-cp                                                    \
-            "./${1}---benchmark-output-parsed.dat"    \
+cp                                                                    \
+            "./${benchmark_dirname}---benchmark-output-parsed.dat"    \
             '../../arena-results'
 
 
--- make-tasks/benchmark-single-metric.sh
+++ make-tasks/benchmark-single-metric.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-cd  "./arena/${1}/"
+benchmark_dirname="${1}"
+title="${2}"
+y_label="${3}"
 
-go  test   -bench=.   |    tee   "./${1}---benchmark-raw-output.dat"
+cd  "./arena/${benchmark_dirname}/"
+
+go  test   -bench=.   |    tee   "./${benchmark_dirname}---benchmark-raw-output.dat"
 
 if [[ ${PIPESTATUS[0]} -gt 0 ]]; then
     exit 1
@@ -10,26 +14,26 @@ fi
 
 awk \
 '/[*][*]/{count++; printf("%d,%s,%s\n", count, $2, $3); }' \
-"./${1}---benchmark-raw-output.dat"                        \
-> "./${1}---benchmark-output-parsed.dat"
+"./${benchmark_dirname}---benchmark-raw-output.dat"        \
+> "./${benchmark_dirname}---benchmark-output-parsed.dat"
 
-# messageSizeMax=`                    awk -F','                         'BEGIN{a=0}{ if ($3>0+a) a=$3} END{print a}'     "./${1}---benchmark-output-parsed.dat"    `
+# messageSizeMax=`                    awk -F','                         'BEGIN{a=0}{ if ($3>0+a) a=$3} END{print a}'     "./${benchmark_dirname}---benchmark-output-parsed.dat"    `
 # messageSizeMaxRoundedUpwards=`      awk -v n="${messageSizeMax}"      'BEGIN{ print int((n+100) / 100 ) * 100 }'                                                 `
 
 tempDir=`mktemp -d -t golang-compression-libs-arena.XXXX`
 tempPlotConfigFile="${tempDir}/plot.gp"
 
-cp            '../plot.gp'                    "${tempPlotConfigFile}"
-sed    -i     "s/___TITLE___/[${1}] ${2}/g"   "${tempPlotConfigFile}"
+cp            '../plot.gp'                                        "${tempPlotConfigFile}"
+sed    -i     "s/___TITLE___/[${benchmark_dirname}] ${title}/g"   "${tempPlotConfigFile}"
 gnuplot \
--e "    file_path='./${1}---benchmark-output-parsed.dat'                                             "    \
--e "    graphic_file_name='../../arena-results/${1}--eventual-message-size-footprint--result.png'    "    \
--e "    y_label='${3}'                                                                               "    \
--e "    column_1=1                                                                                   "    \
--e "    column_2=3                                                                                   "    \
+-e "    file_path='./${benchmark_dirname}---benchmark-output-parsed.dat'                                             "    \
+-e "    graphic_file_name='../../arena-results/${benchmark_dirname}--eventual-message-size-footprint--result.png'    "    \
+-e "    y_label='${y_label}'                                                                                         "    \
+-e "    column_1=1                                                                                                   "    \
+-e "    column_2=3                                                                                                   "    \
 "${tempPlotConfigFile}"
 
-cp       "./${1}---benchmark-output-parsed.dat"   '../../arena-results'
+cp       "./${benchmark_dirname}---benchmark-output-parsed.dat"   '../../arena-results'
 
 echo     'Plots successfully generated';
 
