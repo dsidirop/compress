92c9cebfd740ab5d0a703379ec30fd4fdfd54216
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-02-24 16:00:54 +0100

    task (compile-protobuf): introduce new rule

--- make-tasks/tasks.benchmark.mk
+++ make-tasks/tasks.benchmark.mk
@@ -14,30 +14,35 @@ benchmark:									               \
 
 .PHONY:\
 benchmark-serialization-performance
-benchmark-serialization-performance:
+benchmark-serialization-performance:    compile-protobuf
 	@$(call benchmark-performance,a-serialization-performance,$(cpucount))
 
 .PHONY:\
 benchmark-deserialization-performance
-benchmark-deserialization-performance:
+benchmark-deserialization-performance:    compile-protobuf
 	@$(call benchmark-performance,b-deserialization-performance,$(cpucount))
 
 .PHONY:\
 benchmark-serialization-deserialization-performance
-benchmark-serialization-deserialization-performance:
+benchmark-serialization-deserialization-performance:    compile-protobuf
 	@$(call benchmark-performance,c-serialization-deserialization-performance,$(cpucount))
 
 # .PHONY:\
 # benchmark-serialization-deserialization-elapsed-time
-# benchmark-serialization-deserialization-elapsed-time:
+# benchmark-serialization-deserialization-elapsed-time:    compile-protobuf
 # 	@$(call benchmark-single-metric,d-serialization-deserialization-elapsed-time,Average Elapsed Time in nsecs - Lower is better,ns,$(cpucount))
 
 .PHONY:\
 benchmark-serialization-message-size-footprint
-benchmark-serialization-message-size-footprint:
+benchmark-serialization-message-size-footprint:    compile-protobuf
 	@$(call benchmark-single-metric,e-serialization-eventual-message-size-footprint,Eventual Size in Bytes - Lower is better,bytes,$(cpucount))
 
 .PHONY:\
 merge-output-images-of-plots
 merge-output-images-of-plots: # merge all images into one
 	@convert -append       ./arena-results/*-cpu$(cpucount)----category-overall-results.png      ./arena-results/x-cpu$(cpucount)-all-results.png
+
+compile-protobuf:
+	@cd arena  &&  protoc    --go_out=.    pbfooitem.proto
+	@touch  $@
+	
