b938e2cd91fa672a32d4b8b0be28c897ce5949fa
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2022-03-07 15:53:24 +0100

    feat (decompression and deserialization for protobuf)

---
+++ arena/bb-decompression-deserialization-performance/xee_protobuf_test.go
@@ -0,0 +1,50 @@
+package decompression_with_deserialization_performance
+
+import (
+	"testing"
+
+	"github.com/klauspost/compress/arena"
+	"google.golang.org/protobuf/proto"
+)
+
+func Benchmark___DecompressionAndDeserializationPerformance___Protobuf(b *testing.B) {
+	datasource := arena.SpecialDatasourcesForIDLMechanisms.Protobuf
+	datasourceArrayLength := len(datasource)
+
+	for _, test := range arena.AllCompressionTestCases {
+		b.Run(test.Desc, func(bench *testing.B) {
+			compressedAndSerializedDatasource := [][]byte{} //first serialize and compress
+			for i := 0; i < datasourceArrayLength; i++ {
+				x := datasource[i]
+
+				serializedBytes, err := proto.Marshal(x)
+				if err != nil {
+					panic(err)
+				}
+
+				compressedAndSerializedBytes, err := test.CompressionCallback(serializedBytes)
+				if err != nil {
+					panic(err)
+				}
+
+				compressedAndSerializedDatasource = append(compressedAndSerializedDatasource, compressedAndSerializedBytes)
+			}
+
+			bench.ResetTimer() //vital
+			for i := 0; i < bench.N; i++ {
+				x := compressedAndSerializedDatasource[i%datasourceArrayLength] //and now we deserialize and decompress
+
+				decompressedSerializedBytes, err := test.DecompressionCallback(x)
+				if err != nil {
+					panic(err)
+				}
+
+				item := &arena.PBFooItem{}
+				err = proto.Unmarshal(decompressedSerializedBytes, item)
+				if err != nil {
+					panic(err)
+				}
+			}
+		})
+	}
+}
